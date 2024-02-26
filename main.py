9f7ba2d00ad37a4a1897fbeef3c263e4b76dbf7b
abhiumesh <abhishekmalu14@gmail.com>
2024-02-26 17:01:24 +0530

    Database connectivity Done

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask
+import psycopg2
+from DBcredential import Token
 
-app = Flask(__name__)
+with psycopg2.connect(
+  host=Token.get("hostname"),
+  user=Token.get("username"),
+  password=Token.get("pwd"),
+  database=Token.get("database"),
+  port=Token.get("port_id")
+) as conn:
+    
+    with conn.cursor() as curs:
+        curs.execute('''CREATE TABLE IF NOT EXISTS employee(
+            EmployeeID int,
+            Name varchar(255),
+            Email varchar(255));
+        ''')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+        curs.execute('''
+        INSERT INTO employee (EmployeeID, Name, Email)
+            VALUES (101, 'Mark', 'mark@company.com'),
+                (102, 'Robert', 'robert@company.com'),
+                (103, 'Spencer', 'spencer@company.com');
+        ''')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        delete_script = '''delete from employee where Name = %s'''
+        curs.execute(delete_script,("Mark",))
+        conn.commit()
