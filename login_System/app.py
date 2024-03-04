0d9e9fb4418e4778d043ea73ad09fcf9ce9c5d19
abhiumesh <abhishekmalu14@gmail.com>
2024-03-04 17:38:11 +0530

    typo-fixing and resolve error

--- login_System/app.py
+++ login_System/app.py
@@ -16,15 +16,14 @@ conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_
 
 @app.route('/')
 def Index():
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    s = "select * from register"
-    cur.execute(s) # Execute the SQL
-    list_users = cur.fetchall()
-    print(list_users)
     return render_template('login.html')
-
-@app.route('/register', methods=['POST'])
+    
+@app.route('/register')
 def register():
+    return render_template('register.html')
+
+@app.route('/add_patient', methods=['POST'])
+def add_patient():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     if request.method == 'POST':
         fname = request.form['fname']
@@ -32,11 +31,26 @@ def register():
         email = request.form['email']
         mobileno = request.form['mobileno']
         password = request.form['password']
+        print(fname,lname, email, mobileno, password)
         cur.execute("INSERT INTO register (fname, lname, email, mobileno, password) VALUES (%s,%s,%s,%s,%s)", (fname, lname, email, mobileno, password))
         conn.commit()
-        flash('Patient Added successfully')
         return redirect(url_for('Index'))
-    
+
+@app.route('/login', methods=['POST'])
+def login():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        cur.execute('SELECT * FROM register WHERE email = %s and password = %s', (email, password))
+        data = cur.fetchall()
+        if (len(data)):
+            return redirect(url_for('dashboard'))
+        return redirect(url_for('Index'))
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
